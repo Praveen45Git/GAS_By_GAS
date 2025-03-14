639a8b9fcf96b3bd9dc9a129d0fd9588a5e71082
FathimaNafla14 <fnafla1435@gmailcom>
2025-03-14 23:27:10 +0530

    Nalf payment changes last push

---
+++ Gas_By_Gas_API/Gas_By_Gas_API/Models/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gas_By_Gas_API.Models
+{
+    public class Payment
+    {
+        public long tokenId { get; set; }
+        public long customerId { get; set; }
+        public long Amount { get; set; }
+        public long outletId { get; set; }
+        public decimal emptyQty { get; set; }
+        public decimal amount { get; set; }
+        public decimal payDate { get; set; }
+    }
+}
